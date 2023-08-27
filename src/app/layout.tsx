86903718e3983ca58e6007171cad91e09235b1ba
ReturnReason <reasonz@daum.net>
2023-08-27 22:26:32 +0900

    feat: getJobs 프리패치되도록 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import ClientProvider from "@/components/client/ClientProvider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { IBM_Plex_Sans_KR } from "next/font/google";
+import { getJobs } from "@/service/getJobs";
+import { dehydrate } from "@tanstack/react-query";
+import HydrateOnClient from "@/store/hydrateOnClient";
+import getQueryClient from "@/store";
 
 const ibmKr = IBM_Plex_Sans_KR({
   subsets: ["latin"],
@@ -16,15 +20,25 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // TODO: 코드 이동시키기
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["getJobs"],
+    queryFn: getJobs,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <html lang="ko">
       <body className={ibmKr.className}>
-        <ClientProvider>{children}</ClientProvider>
+        <ClientProvider>
+          <HydrateOnClient state={dehydratedState}>{children}</HydrateOnClient>
+        </ClientProvider>
       </body>
     </html>
   );
