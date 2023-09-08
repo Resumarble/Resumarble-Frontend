dbfe983c432754be9c059b4ff2f6b65fea3e60b9
ReturnReason <reasonz@daum.net>
2023-09-08 22:36:03 +0900

    design: 저장 버튼 애니메이션 추가

--- src/app/result/components/SaveButton.tsx
+++ src/app/result/components/SaveButton.tsx
@@ -5,7 +5,9 @@ import { FiSave } from "react-icons/fi";
 export default function SaveButton({ txt }: { txt: string }) {
   return (
     <div className={styles.container} onClick={() => downloadAsTxt(txt)}>
-      <FiSave size={23} />
+      <div>
+        <FiSave size={23} />
+      </div>
     </div>
   );
 }
