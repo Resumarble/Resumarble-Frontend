b4b0ed0050b834951297528d9dc6594f12a446d8
ReturnReason <reasonz@daum.net>
2023-09-29 21:17:52 +0900

    feat(Button): 버튼 컴포넌트 클래스네임 프롭스가 없을 때 삼항처리로 변경, onClick, type 프롭스 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,22 +1,30 @@
 import styles from "./button.module.css";
 
 interface ButtonProps {
+  type?: string;
   children: React.ReactNode;
   className?: string;
   props?: any;
   isDark?: boolean;
+  onClick?: (e: React.FormEvent | React.MouseEvent) => void;
 }
 
 export default function Button({
   children,
   className,
   props,
+  onClick,
+  type = "button",
   isDark = false,
 }: ButtonProps) {
   return (
     <>
       <button
-        className={`${className} ${styles.button} ${isDark ? styles.dark : ""}`}
+        onClick={onClick}
+        type={type}
+        className={`${className ? className : ""} ${styles.button} ${
+          isDark ? styles.dark : ""
+        }`}
         {...props}
       >
         {children}
