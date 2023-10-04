1b79fed49bbb67cb166f4753b69b3a330d568542
ReturnReason <reasonz@daum.net>
2023-10-04 23:14:15 +0900

    feat: 리프레시 토큰 API 추가

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -1,14 +1,13 @@
+import createDecodedToken from "@/utils/createDecodedToken";
 import { NextResponse } from "next/server";
 
-const jwt = require("jsonwebtoken");
-
-// 토큰 복호화
+// 토큰 복호화 후 유저 id 반환
 export async function POST(req: Request) {
   const res = await req.json();
 
   const token = res.token;
-  const secretKey = process.env.NEXT_PUBLIC_SECRET_KEY;
-  const decodedToken = jwt.verify(token, secretKey);
+  const decodedToken = createDecodedToken(token);
+
   const userId = decodedToken.id;
 
   return NextResponse.json({ id: userId });
