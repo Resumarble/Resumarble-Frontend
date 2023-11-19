dde6fca603acfee9cb557170224cf589bea7527f
ReturnReason <reasonz@daum.net>
2023-11-19 18:11:19 +0900

    design(ToggleBox): 스타일링 수정

--- src/components/common/ToggleBox.tsx
+++ src/components/common/ToggleBox.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import styles from "./toggleBox.module.css";
+import { useState } from 'react';
+import styles from './toggleBox.module.css';
 
 type Props = {
   title: string;
@@ -15,25 +15,25 @@ export default function ToggleBox({
   children,
 }: Props): JSX.Element {
   const [isShow, setIsShow] = useState(false);
-  const [arrowDirection, setArrowDirection] = useState("down");
+  const [arrowDirection, setArrowDirection] = useState('down');
 
   const handleTitleClick = () => {
     setIsShow((prev) => !prev);
     setArrowDirection((prev) => {
-      return prev === "down" ? "up" : "down";
+      return prev === 'down' ? 'up' : 'down';
     });
   };
 
   return (
     <div className={styles.container}>
-      <h2 onClick={handleTitleClick} className={styles.title}>
+      <div onClick={handleTitleClick} className={styles.toggleBox}>
         {children}
-        {title}
+        <h2>{title}</h2>
         <i
           className={`${styles.arrow} ${styles[arrowDirection]}`}
-          role="img"
+          role='img'
         ></i>
-      </h2>
+      </div>
 
       {isShow && (
         <p className={`${styles.contents} ${isShow && styles.show}`}>
