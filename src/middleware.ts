50771ccb7733b9572618d20cb93d9313e624091d
ReturnReason <reasonz@daum.net>
2023-12-03 13:53:19 +0900

    feat: 로그인, 비로그인 유저 접근 권한 미들웨어 세팅

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,26 @@
-import { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest) {
-  // TODO 리프레시 토큰 로직 어디에 넣을지 고민......
-  // - request 응답 코드가 401인 경우 재발급하도록하려면 패치 중간에 로직이 필요함
-  // -> 이거 때문에 axios를 사용할 필요가 있을까 ?
-  // -> fetch 메서드 인터셉터를 추가하는 방향으로 ? (커스텀 fetch)
-  // const now = Math.floor(Date.now() / 1000);
-  // const isExpired = (token.exp as number) - now < 600; // 만료 10분 전인지 체크
-  // TODO middleware가 서버 사이드라서 getSession 불가능.
-  // 그렇다면 어떤 방법으로 회원/비회원을 구분할 것인가..
+export default async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (token && token.accessToken) {
+    if (req.nextUrl.pathname.startsWith('/login')) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+
+    if (req.nextUrl.pathname.startsWith('/join')) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+  }
+
+  if (!token) {
+    if (req.nextUrl.pathname.startsWith('/mypage')) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+  }
 }
 
 export const config = {
-  matcher: ['/join', '/login'],
+  matcher: ['/mypage', '/login', '/join'],
 };
