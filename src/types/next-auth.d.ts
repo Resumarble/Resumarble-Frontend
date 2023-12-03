cbf040693e58a88feb278ef769bf35d4efec4f83
ReturnReason <reasonz@daum.net>
2023-12-03 13:53:42 +0900

    feat: JWT 타입 extends 하여 타입 재설정

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,13 @@
 import { DefaultSession } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 
+interface ExtendedJWT extends JWT {
+  accessToken?: string;
+  refreshToken?: string;
+  id?: number;
+}
 declare module 'next-auth' {
   interface Session {
-    accessToken?: string;
-    refreshToken?: string;
-    id?: number;
+    user: ExtendedJWT;
   }
 }
