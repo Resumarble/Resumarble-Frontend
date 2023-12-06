c44d6badfeac16c7f4291a14d60d38df7b2a6f88
ReturnReason <reasonz@daum.net>
2023-12-07 00:10:07 +0900

    feat: useMypageInfiniteQuery 로직 추가

--- src/store/react-query/hooks/mypage/index.ts
+++ src/store/react-query/hooks/mypage/index.ts
@@ -1,20 +1,34 @@
 import customFetch from '@/utils/customFetch';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { Session } from 'next-auth';
 
+export const useMypageInfiniteQuery = () => {
+  return useInfiniteQuery({
+    queryKey: ['mypage'],
+    queryFn: async ({ pageParam = 0 }) => {
+      const res = await fetch(`/api/mock?page=${pageParam}`);
+      const data = await res.json();
+      return data;
+    },
+    getNextPageParam: (page, allPages) => {
+      return page.hasNextPage ? allPages.length : undefined;
+    },
+  });
+};
+
+// TODO  아래 코드는 필요없으면 제거
 export const useMypageQuery = (session: Session) => {
   return useQuery({
-    queryKey: ['getMyPage', session?.user.id],
+    queryKey: ['mypage', session?.user.id],
     queryFn: async () => {
       const data = await customFetch({
-        // url: `/predictions/${userId}`,
         url: '/users/me',
         method: 'GET',
-        params: '2',
+        params: `?page=${2}`,
       });
 
       return data.data.predictions;
     },
-    enabled: !!(session?.user.id! >= 0),
+    enabled: !!(session?.user.id! >= 0), //  * enabled 왜 넣었었지 ?
   });
 };
