5ec537e6a0493615a323673fb429536ffe12336f
ReturnReason <reasonz@daum.net>
2023-12-21 23:33:44 +0900

    feat: Button 스토리 파일 추가

---
+++ src/components/common/Button.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from './Button';
+
+const meta = {
+  title: 'Common/Button',
+  component: Button,
+  argTypes: {
+    label: {
+      control: 'text',
+    },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {
+    label: 'basic',
+    variant: 'default',
+    type: 'button',
+  },
+};
+
+export const Dark: Story = {
+  args: {
+    label: 'Dark',
+    variant: 'dark',
+    type: 'button',
+  },
+};
