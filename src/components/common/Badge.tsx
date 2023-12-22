8b3736fefbee3786302bc951e1a5943932068580
ReturnReason <reasonz@daum.net>
2023-12-22 20:13:12 +0900

    refactor(Badge): 뱃지 컴포넌트 리팩토링

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -1,14 +1,21 @@
-import styles from "./badge.module.css";
+import styles from './badge.module.css';
 
 interface Props {
-  children?: React.ReactNode;
   text: string;
+  textColor?: 'white' | 'black';
+  backgroundColor?: string;
 }
 
-export default function Badge({ children, text }: Props) {
+export default function Badge({
+  text,
+  textColor = 'white',
+  backgroundColor = '#5f946c50',
+}: Props) {
+  const style = { backgroundColor, color: `var(--${textColor})` };
+
   return (
-    <div className={styles.container}>
-      <h5 className={styles.text}>{text}</h5>
+    <div className={styles.container} style={style}>
+      <p className={styles.text}>{text}</p>
     </div>
   );
 }
