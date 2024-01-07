75362f345d90ff552bc35ebfa27e7ca52e1d4fc9
ReturnReason <reasonz@daum.net>
2024-01-07 15:21:03 +0900

    feat: 임시 페이지 추가

---
+++ src/app/(public)/resume/feedback/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+import Container from '@/components/common/Container';
+
+export default function FeedbackPage() {
+  return (
+    <Container>
+      <p>준비중입니다. </p>
+      <br />
+      <Link href='/'>메인으로</Link>
+    </Container>
+  );
+}
