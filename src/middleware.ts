fabc3b374a85d2fe7b644fb411133b912d478395
ReturnReason <reasonz@daum.net>
2024-01-20 19:41:35 +0900

    refactor(middleware): if문 조건 중복로직 함수 분리

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,25 @@ import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 import { AUTH_URL } from './constants/path';
 
+const checkPathName = (req: NextRequest, path: string) => {
+  return req.nextUrl.pathname.startsWith(path);
+};
+
 export default async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   if (token && token.accessToken) {
-    if (req.nextUrl.pathname.startsWith(`${AUTH_URL.LOGIN}`)) {
+    if (checkPathName(req, `${AUTH_URL.LOGIN}`)) {
       return NextResponse.redirect(new URL('/', req.url));
     }
 
-    if (req.nextUrl.pathname.startsWith(`${AUTH_URL.JOIN}`)) {
+    if (checkPathName(req, `${AUTH_URL.JOIN}`)) {
       return NextResponse.redirect(new URL('/', req.url));
     }
   }
 
   if (!token) {
-    if (req.nextUrl.pathname.startsWith('/mypage')) {
+    if (checkPathName(req, `/mypage`)) {
       return NextResponse.redirect(new URL('/', req.url));
     }
   }
