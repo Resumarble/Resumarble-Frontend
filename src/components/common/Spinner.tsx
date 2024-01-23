2d4b67280c3fadd0e810eb94306341712aec83d4
ReturnReason <reasonz@daum.net>
2024-01-23 17:36:58 +0900

    refactor(Spinner): 스피너 컴포넌트 중복 엘리먼트 반복문으로 변경

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -1,54 +1,18 @@
-import styles from "./spinner.module.css";
+import styles from './spinner.module.css';
 
 export default function Spinner() {
   return (
     <div className={styles.loadingSpinner}>
       <div className={styles.spin}>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
+        {Array(15)
+          .fill(0)
+          .map((_, index) => {
+            return (
+              <div key={index}>
+                <div></div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
